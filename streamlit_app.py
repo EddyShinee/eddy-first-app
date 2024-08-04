83e82c9c32ecb798f86817175df19b064ca10551
Eddy <thuanvd.uit@gmail.com>
2024-08-05 00:36:48 +0700

    aaa

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,6 @@
 import streamlit as st
+import pandas as pd
+
 
 st.title("🎈 My new app")
 st.write(
@@ -13,10 +15,49 @@ st.subheader("Four", divider=True)
 
 st.write("This is some text.")
 
-st.slider("This is a slider", 0, 100, (25, 75))
+st.slider("This is a slider", 0, 100, (0, 25))
 
-st.divider()  # 👈 Draws a horizontal rule
+def get_user_name():
+    return 'John'
 
-st.write("This text is between the horizontal rules.")
+# ------------------------------------------------
+# Want people to see this part of the code...
+
+def get_punctuation():
+    return '!!!'
+
+greeting = "Hi there, "
+user_name = get_user_name()
+punctuation = get_punctuation()
+
+st.write(greeting, user_name, punctuation)
+foo = 'bar'
+st.write('Done!')
 
 st.divider()
+df = pd.DataFrame(
+    [
+        {"command": "st.selectbox", "rating": 4, "is_widget": True},
+        {"command": "st.balloons", "rating": 5, "is_widget": False},
+        {"command": "st.time_input", "rating": 3, "is_widget": True},
+    ]
+)
+
+edited_df = st.data_editor(
+    df,
+    column_config={
+        "command": "Streamlit Command",
+        "rating": st.column_config.NumberColumn(
+            "Your rating",
+            help="How much do you like this command (1-5)?",
+            min_value=1,
+            max_value=5,
+            step=1,
+            format="%d ⭐",
+        ),
+        "is_widget": "Widget ?",
+    },
+    disabled=["command", "is_widget"],
+    hide_index=True,
+)
+st.subheader("end", divider=True)
