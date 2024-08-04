4fa4e8232a022708eace44aed4b11b849bdcdd27
Eddy <thuanvd.uit@gmail.com>
2024-08-05 00:22:03 +0700

    aaa

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,19 @@ st.title("🎈 My new app")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+
+st.write("Xin chào, đây là ứng dụng đầu tiên của tôi")
+
+
+st.write("thật là tuyệt")
+st.subheader("Four", divider=True)
+
+st.write("This is some text.")
+
+st.slider("This is a slider", 0, 100, (25, 75))
+
+st.divider()  # 👈 Draws a horizontal rule
+
+st.write("This text is between the horizontal rules.")
+
+st.divider()
