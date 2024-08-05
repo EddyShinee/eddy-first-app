e8dd9f9cc986e2d356d92096196fb2cdd941c2bb
Eddy <thuanvd.uit@gmail.com>
2024-08-05 18:08:31 +0700

    aaa

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 
 
-st.title("🎈 My new app")
+st.title("Eddy Dashboard")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
