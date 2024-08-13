01e8b34de5f5e11b5b3313baae36796238c8d870
Eddy <thuanvd.uit@gmail.com>
2024-08-13 16:55:31 +0700

    Add new feature

---
+++ Authentication/Login/Login.py
@@ -0,0 +1,20 @@
+import streamlit as st
+from Utils.Auth import validate_login
+
+def login(cookies):
+    st.title('Login')
+    login_container = st.empty()
+    st.markdown('<div class="login-container">', unsafe_allow_html=True)
+    with login_container.container():
+        username = st.text_input('Username')
+        password = st.text_input('Password', type='password')
+        if st.button('Login'):
+            if validate_login(username, password):
+                st.session_state['logged_in'] = True
+                cookies["logged_in"] = "true"
+                cookies["Username"] = username
+                cookies.save()
+                st.rerun()  # Refresh the page to update the state
+            else:
+                st.error('Invalid username or password')
+    st.markdown('</div>', unsafe_allow_html=True)
--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,11 @@
+import os
 import streamlit as st
+from streamlit_cookies_manager import EncryptedCookieManager
+
 from Utils.Auth import validate_login
+from SideBar.SideBar import sidebar
+from Authentication.Login.Login import login  # Import hàm login từ tệp Login.py
+
 import Pages.Home.Home as home
 import Pages.About.About as about
 import Pages.Contact.Contact as contact
@@ -7,11 +13,25 @@ import Pages.AccountMetaTrader.AccountMetaTrader as accountmt
 import Pages.Dashboard.Dashboard as db
 import Pages.MetaTrader.MT4.MT4 as MT4
 import Pages.MetaTrader.MT5.MT5 as MT5
-from streamlit_cookies_manager import EncryptedCookieManager
-from SideBar.SideBar import sidebar
+
+# Function to load CSS
+def load_css(file_name):
+    with open(file_name) as f:
+        st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
 
 # Set page configuration to use the full width
-st.set_page_config(page_title="Eddy Shinee", layout="wide")
+st.set_page_config(
+    page_title="Eddy - Adam Project",
+    page_icon=":shark:",  # Can be an emoji or a URL to an image
+    layout="wide",  # Options: "centered", "wide"
+    initial_sidebar_state="auto",  # Options: "auto", "expanded", "collapsed"
+    menu_items={
+        'Get Help': 'https://www.extremelycoolapp.com/help',
+        'Report a bug': 'https://www.extremelycoolapp.com/bug',
+        'About': "# This is a header. This is an *extremely* cool app!"
+    }
+)
+
 # Create a cookie manager
 cookies = EncryptedCookieManager(
     prefix="myapp_",  # prefix for cookie names
@@ -21,6 +41,7 @@ cookies = EncryptedCookieManager(
 if not cookies.ready():
     st.stop()
 
+# Main function to control the app flow
 def main():
     if 'logged_in' not in st.session_state:
         # Check if there's a login cookie
@@ -32,10 +53,9 @@ def main():
             st.session_state['page'] = 'Home'
 
     if not st.session_state['logged_in']:
-        login()
+        login(cookies)  # Gọi hàm login từ file Login.py
     else:
         page = sidebar(cookies)
-        print(page)
         # Use a dictionary to switch between pages
         pages = {
             'Home': home.app,
@@ -49,24 +69,6 @@ def main():
         # Call the function corresponding to the current page
         if page in pages:
             pages[page]()
-
-
-
-def login():
-    st.title('Login')
-    username = st.text_input('Username')
-    password = st.text_input('Password', type='password')
-    if st.button('Login'):
-        if validate_login(username, password):
-            st.session_state['logged_in'] = True
-            cookies["logged_in"] = "true"
-            cookies["Username"] = username
-            cookies.save()
-            st.rerun()
-            # Refresh the page to update the state
-            # st.experimental_rerun()
-        else:
-            st.error('Invalid username or password')
 
 if __name__ == '__main__':
     main()
